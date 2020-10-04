4749c4b095ce83d9343c6ad6e325eebb4bec57ad
Ivan Kozin <rosonero6140@gmail.com>
2020-10-04 15:59:35 +0300

    ninth commit (googleMap ready)

--- app/src/main/java/com/kozin/weatherkotlin/ui/fragments/CurrentWeatherFragment.kt
+++ app/src/main/java/com/kozin/weatherkotlin/ui/fragments/CurrentWeatherFragment.kt
@@ -1,12 +1,10 @@
 package com.kozin.weatherkotlin.ui.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.google.android.gms.common.api.Status
--- app/src/main/java/com/kozin/weatherkotlin/ui/fragments/GoogleMapFragment.kt
+++ app/src/main/java/com/kozin/weatherkotlin/ui/fragments/GoogleMapFragment.kt
@@ -5,10 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
@@ -19,19 +18,18 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.kozin.weatherkotlin.R
 import com.kozin.weatherkotlin.databinding.FragmentGoogleMapBinding
-import com.kozin.weatherkotlin.ui.viewModel.CurrentWeatherViewModel
 import com.kozin.weatherkotlin.ui.viewModel.GoogleMapViewModel
 import com.kozin.weatherkotlin.utils.Resource
 import com.kozin.weatherkotlin.utils.SessionManager
-import kotlinx.android.synthetic.main.fragment_current_weather.*
 
 class GoogleMapFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
 
-    var map: GoogleMap? = null
-    var marker1: Marker? = null
+    private lateinit var map: GoogleMap
+    private lateinit var marker1: Marker
 
     private var _binding: FragmentGoogleMapBinding? = null
     private val binding get() = _binding!!
+
     private lateinit var viewModel: GoogleMapViewModel
     private lateinit var sessionManager: SessionManager
 
@@ -54,46 +52,17 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener 
     override fun onMapReady(p0: GoogleMap?) {
         setUpViewModel()
         p0?.apply {
-            map = p0
-
-            map!!.setOnMapClickListener { latLng ->
-                map!!.clear()
-                marker1 = map!!.addMarker(
+            p0.setOnMapClickListener { latLng ->
+                p0.clear()
+                marker1 = p0.addMarker(
                     MarkerOptions()
                         .position(latLng)
                         .title(latLng.latitude.toString() + " : " + latLng.longitude)
                         .snippet("This is my spot!")
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
                 )
-                map!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
-
-                getCityName()
-
-//                    Log.i("MAS", marker1?.position?.latitude.toString() + " " +
-//                            marker1?.position?.longitude.toString())
-//                    viewModel.getCurrentWeatherByLatLng(marker1?.position?.latitude.toString(),
-//                        marker1?.position?.longitude.toString(),
-//                        "en", "metric")
-//                        .observe(viewLifecycleOwner, {
-//                            it?.let {resource ->
-//                                when (resource.status) {
-//                                    Resource.Status.SUCCESS -> {
-//                                        it.data?.let { currentWeather ->
-//                                            var cityName: String = currentWeather.data!!.name
-//                                            sessionManager.saveCityLatLng(cityName)
-//                                            Log.i("MAS", cityName)
-//                                        }
-//                                    }
-//                                    Resource.Status.ERROR -> {
-//                                        Log.i("MAS", "OSHIBKA")
-//                                    }
-//                                    Resource.Status.LOADING -> {
-//                                        Log.i("MAS", "LOADING")
-//                                    }
-//                                }
-//                            }
-//                        })
-
+                p0.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                p0.setOnMarkerClickListener(this@GoogleMapFragment)
 
                 }
 
@@ -101,22 +70,24 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener 
             }
         }
 
-    override fun onMarkerClick(p0: Marker?): Boolean = false
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        getCityName()
+        return false
+    }
 
     private fun getCityName(){
-        Log.i("MAS", marker1?.position?.latitude.toString() + " " +
-                marker1?.position?.longitude.toString())
-        viewModel.getCurrentWeatherByLatLng(marker1?.position?.latitude.toString(),
-            marker1?.position?.longitude.toString(),
+        viewModel.getCurrentWeatherByLatLng(marker1.position?.latitude.toString(),
+            marker1.position?.longitude.toString(),
             "en", "metric")
             .observe(viewLifecycleOwner, {
                 it?.let {resource ->
                     when (resource.status) {
                         Resource.Status.SUCCESS -> {
                             it.data?.let { currentWeather ->
-                                var cityName: String = currentWeather.data!!.name
+                                val cityName: String = currentWeather.data!!.name
                                 sessionManager.saveCityLatLng(cityName)
                                 Log.i("MAS", cityName)
+                                findNavController().navigate(R.id.action_googleMapFragment_to_currentWeatherFragment2)
                             }
                         }
                         Resource.Status.ERROR -> {
@@ -130,78 +101,3 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener 
             })
     }
 }
-
-//    override fun onMarkerClick(p0: Marker?): Boolean {
-//        if (p0?.equals(marker1)!!){
-//            Log.i("MAS", p0?.position?.latitude.toString() + " " +
-//                    p0?.position?.longitude.toString())
-//        }else{
-//            Log.i("MAS", "FUK U")
-//        }
-//
-//        when(p0 == marker1){ true -> {
-//            Log.i("MAS", marker1?.position?.latitude.toString() + " " +
-//                    marker1?.position?.longitude.toString())
-//            viewModel.getCurrentWeatherByLatLng(marker1?.position?.latitude.toString(),
-//                marker1?.position?.longitude.toString(),
-//                "en", "metric")
-//                .observe(viewLifecycleOwner, {
-//                    it?.let {resource ->
-//                        when (resource.status) {
-//                            Resource.Status.SUCCESS -> {
-//                                it.data?.let { currentWeather ->
-//                                    var cityName: String = currentWeather.data!!.name
-//                                    sessionManager.saveCityLatLng(cityName)
-//                                    Log.i("MAS", cityName)
-//                                }
-//                            }
-//                            Resource.Status.ERROR -> {
-//                                Log.i("MAS", "OSHIBKA")
-//                            }
-//                            Resource.Status.LOADING -> {
-//                                Log.i("MAS", "LOADING")
-//                            }
-//                        }
-//                    }
-//                })
-//        }
-//          false -> {
-//              Log.i("TRAP", "MISS")
-//
-//          }
-//        }
-
-//        Log.i("MAS", marker1?.position?.latitude.toString() + " " +
-//            marker1?.position?.longitude.toString())
-
-
-
-//        return false
-//    }
-
-//    private fun refreshData() {
-//        viewModel.getCurrentWeatherByLatLng(marker1?.position?.latitude.toString(),
-//            marker1?.position?.longitude.toString(),
-//            "en", "metric")
-//            .observe(viewLifecycleOwner, {
-//            it?.let {resource ->
-//                when (resource.status) {
-//                    Resource.Status.SUCCESS -> {
-//                        it.data?.let { currentWeather ->
-//                            var cityName: String = currentWeather.data!!.name
-//                            sessionManager.saveCityLatLng(cityName)
-//
-//                        }
-//                    }
-//                    Resource.Status.ERROR -> {
-//
-//                    }
-//                    Resource.Status.LOADING -> {
-//
-//                    }
-//                }
-//            }
-//        })
-//    }
-
-//}
